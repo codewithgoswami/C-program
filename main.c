ffd193656d9ecf6f40ced1fd9b2f8fe5d3902f5e
kuldeep goswami <goswamikuldeep138@gmail.com>
2026-07-19 22:49:06 +0530

    main file

---
+++ main.c
@@ -0,0 +1,9 @@
+// Constants are using for headlines and highlight it is denoted by-- (//) single line comment  or (/* */) multiple line comments
+
+// New line-- (\n)
+
+// for degree symbol we can hold alt and press 0176 and degree symbol how (°)
+
+
+// Variables--
+
